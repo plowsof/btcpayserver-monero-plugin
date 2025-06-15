def4297ba3b96003776db0fdd578f3407515ff67
plowsof <plowsof@protonmail.com>
2025-06-15 18:48:19 +0100

    sanity check

--- BTCPayServer.Plugins.IntegrationTests/Monero/MoneroPluginUITest.cs
+++ BTCPayServer.Plugins.IntegrationTests/Monero/MoneroPluginUITest.cs
@@ -15,7 +15,6 @@ public class MoneroPluginIntegrationTest(ITestOutputHelper helper) : MoneroAndBi
         await s.RegisterNewUser(true);
         await s.CreateNewStore();
         await s.Page.Locator("a.nav-link[href*='monerolike/XMR']").ClickAsync();
-        await s.Page.Locator("#NewAccountLabel").WaitForAsync(new() { Timeout = 999999999 }); //plssssssssssssss
         await s.Page.Locator("#NewAccountLabel").FillAsync("Wallet Label"); 
         await s.Page.CheckAsync("#Enabled");
         await s.Page.SelectOptionAsync("#SettlementConfirmationThresholdChoice", "2");
